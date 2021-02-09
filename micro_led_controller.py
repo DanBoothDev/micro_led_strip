0b839d3f535d0a52c781cddba981f6236a630476
Dan Booth <DanBoothDev@gmail.com>
2021-02-08 20:01:25 -0400

    Updated led controller to use init params and include a stop method

--- micro_led_controller.py
+++ micro_led_controller.py
@@ -5,18 +5,19 @@ from machine import Pin, PWM
 class MicroLedController:
     def __init__(self, pin_red=12, pin_green=13, pin_blue=14, frequency=1000, duty=512):
         # led pins
-        self.pin_red = 12
-        self.pin_green = 13
-        self.pin_blue = 14
+        self.pin_red = pin_red
+        self.pin_green = pin_green
+        self.pin_blue = pin_blue
         # pwm leds
         self._led_pwm_red = None
         self._led_pwm_green = None
         self._led_pwm_blue = None
+        # init LEDs
         self._led_init(pin_red, pin_green, pin_blue, frequency, duty)
 
     def _led_init(self, pin_red, pin_green, pin_blue, frequency, duty):
         """
-        De-initialized all LEDs
+        Initialized all LEDs
         """
         self._led_pwm_red = PWM(Pin(pin_red, Pin.OUT), freq=frequency, duty=duty)
         self._led_pwm_green = PWM(Pin(pin_green, Pin.OUT), freq=frequency, duty=duty)
@@ -46,3 +47,6 @@ class MicroLedController:
         for duty_cycle in range(0, 1024):
             self.set_rgb(duty_cycle, duty_cycle, duty_cycle)
             sleep(0.005)
+
+    def stop(self):
+        self._led_deinit()
